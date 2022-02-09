c73e3becb576acd78ba7497f6e357ff4ee1993cb
michael oates <moates@gmail.com>
2022-02-10 08:05:07 +1100

    all code tested

--- robotCommander.test.js
+++
@@ -1,5 +0,0 @@
-const chai = require("chai");
-const expect = chai.expect;
-const commandInterpreter = require("./commandInterpreter");
-const robotCommander = require("./robotCommander");
-const RobotController = require("./RobotController");
