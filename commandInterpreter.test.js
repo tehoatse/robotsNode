a45ddaa9b78f56985c538678a2271ee08dd3f0f3
michael oates <moates@gmail.com>
2022-02-09 15:10:58 +1100

    update tests

--- commandInterpreter.test.js
+++ commandInterpreter.test.js
@@ -4,11 +4,11 @@ const commandInterpreter = require("./commandInterpreter");
 const robotCommander = require("./robotCommander");
 const RobotController = require("./RobotController");
 
-describe("command", () => {
-    before(() => {
-        const controller = new RobotController();
-        commandInterpreter.setController(controller);
-        robotCommander.setController(controller);
+describe("commandInterpreter", () => {
+    beforeEach(() => {
+        this.controller = new RobotController();
+        commandInterpreter.setController(this.controller);
+        robotCommander.setController(this.controller);
     });
     context("interpret", () => {
         it("should return false from nonsense", () => {
@@ -35,5 +35,54 @@ describe("command", () => {
                 true
             );
         });
+        it("should return false from out of bounds place commands", () => {
+            expect(
+                commandInterpreter.interpret(
+                    `place ${this.controller.BOTTOM_BOUNDARY - 1},0,north`
+                )
+            ).to.equal(false);
+            expect(
+                commandInterpreter.interpret(
+                    `place 0,${this.controller.LEFT_BOUNDARY - 1},north`
+                )
+            ).to.equal(false);
+            expect(
+                commandInterpreter.interpret(
+                    `place ${this.controller.TOP_BOUNDARY + 1},0,north`
+                )
+            ).to.equal(false);
+            expect(
+                commandInterpreter.interpret(
+                    `place 0,${this.controller.RIGHT_BOUNDARY + 1},north`
+                )
+            ).to.equal(false);
+        });
+        it("should turn a robot left", () => {
+            commandInterpreter.interpret("place 0,0,north");
+            expect(commandInterpreter.interpret("left")).to.equal(true);
+            expect(this.controller.activeRobot.facing).to.equal("WEST");
+            commandInterpreter.interpret("left");
+            expect(this.controller.activeRobot.facing).to.equal("SOUTH");
+            commandInterpreter.interpret("left");
+            expect(this.controller.activeRobot.facing).to.equal("EAST");
+            commandInterpreter.interpret("left");
+            expect(this.controller.activeRobot.facing).to.equal("NORTH");
+        });
+        it("should turn a robot right", () => {
+            commandInterpreter.interpret("place 0,0,north");
+            expect(commandInterpreter.interpret("right")).to.equal(true);
+            expect(this.controller.activeRobot.facing).to.equal("EAST");
+            commandInterpreter.interpret("right");
+            expect(this.controller.activeRobot.facing).to.equal("SOUTH");
+            commandInterpreter.interpret("right");
+            expect(this.controller.activeRobot.facing).to.equal("WEST");
+            commandInterpreter.interpret("right");
+            expect(this.controller.activeRobot.facing).to.equal("NORTH");
+        });
+        it("should report"),
+            () => {
+                commandInterpreter.interpret("place 0,0,north");
+                expect(commandInterpreter.interpret("report")).to.equal(true);
+            };
     });
 });
