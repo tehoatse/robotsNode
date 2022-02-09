e0da7fd87c4dc1054948dda064bc5e442970913c
michael oates <moates@gmail.com>
2022-02-10 07:39:16 +1100

    clean up white space

--- directions.js
+++ directions.js
@@ -1,40 +1,34 @@
-const directionArray = [
-    'NORTH',
-    'EAST',
-    'SOUTH',
-    'WEST'
-]
+const directionArray = ["NORTH", "EAST", "SOUTH", "WEST"];
 
 module.exports.NORTH = directionArray[0];
 module.exports.EAST = directionArray[1];
 module.exports.SOUTH = directionArray[2];
 module.exports.WEST = directionArray[3];
 
-module.exports.left = function(currentDirection){
+module.exports.left = function (currentDirection) {
     return updateDirection(currentDirection, -1);
-}
+};
 
-module.exports.right = function(currentDirection){
+module.exports.right = function (currentDirection) {
     return updateDirection(currentDirection, 1);
-}
+};
 
-module.exports.isReal = function(potentialDirection){
+module.exports.isReal = function (potentialDirection) {
     return directionArray.includes(potentialDirection);
-}
+};
 
-function updateDirection(currentDirection, increment){
+function updateDirection(currentDirection, increment) {
     let newIndex = directionArray.indexOf(currentDirection) + increment;
-    
+
     //turn right
-    if(newIndex >= directionArray.length){
+    if (newIndex >= directionArray.length) {
         newIndex = newIndex - directionArray.length;
     }
 
     //turn left
-    if(newIndex < 0){
+    if (newIndex < 0) {
         newIndex = directionArray.length + newIndex;
     }
-    
+
     return directionArray[newIndex];
 }
-
